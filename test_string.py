f32d75aa3cf77fcd5f879574ab858ae8c2708640
Adrian Aro <aroadrianchristopher@gmail.com>
2025-02-07 18:53:16 +0800

    add main object

--- test_string.py
+++ test_string.py
@@ -6,8 +6,14 @@ class Person:
     def greet(self):
         print(f'Hellow my name is {self.name} and I am {self.age} years old')
         
-person1 = Person('Reslychen', 26)
-person1.greet()
 
-person2 = Person('Adrian', 25)
-person2.greet()
+def main():
+        person1 = Person('Reslychen', 26)
+        person1.greet()
+
+        person2 = Person('Adrian', 25)
+        person2.greet()
+
+if __name__ == "__main__":
+
+        main()
