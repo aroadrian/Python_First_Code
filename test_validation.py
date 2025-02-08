164f5530ec9f8bf505c679fc1339c4490484eb2c
Adrian Aro <aroadrianchristopher@gmail.com>
2025-02-08 14:12:55 +0800

    edit some context

---
+++ test_validation.py
@@ -0,0 +1,18 @@
+class User:
+    user_count =0
+    
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        User.user_count += 1
+        
+    def display_user(self):
+        print(f'Username: {self.username}, Email: {self.email}')
+
+user1 = User('Adrian', 'adrian@gmail.com')
+user2 = User('Reslychen', 'reslychen@gmail.com')
+
+print(User.user_count)
+print(user1.user_count)
+print(user2.user_count)
+
