2b051dfed050f4cd960bbc885b770e97fd16a0a2
Adrian Aro <aroadrianchristopher@gmail.com>
2025-02-10 22:26:51 +0800

    try to make shorter the code

--- basic_calculator.py
+++ basic_calculator.py
@@ -6,26 +6,44 @@ def main():
    print("3. Multiply")
    print("4. Divide")
 
-   operation = input()
+   operation = int(input("Enter operation: "))
 
-   if operation == '1':
-       num1 = input("Enter first number: ")
-       num2 = input("Enter second number: ")
-       print("The sum is: " + str(int(num1) + int(num2)))
-   elif operation == '2':
-       num1 = input("Enter first number: ")
-       num2 = input("Enter second number: ")
-       print("The sum is: " + str(int(num1) - int(num2)))
-   elif operation == '3':
-       num1 = input("Enter first number: ")
-       num2 = input("Enter second number: ")
-       print("The sum is: " + str(int(num1) *  int(num2)))
-   elif operation == '4':
-       num1 = input("Enter first number: ")
-       num2 = input("Enter second number: ")
-       print("The sum is: " + str(int(num1) / int(num2))) 
+   if operation in [1,2,3,4]:
+        num1 = input("Enter first number: ")
+        num2 = input("Enter second number: ")
+        if operation == 1:
+            result = str(int(num1) + int(num2))
+        elif operation == 2:
+            result = str(int(num1) - int(num2))
+        elif operation == 3:
+            result = str(int(num1) * int(num2))
+        elif operation == 4:
+            result = str(int(num1) // int(num2))
+        print("The result is: {} " .format(result))
    else:
-       print("Invalid operation")   
+        print("Invalid operation")
+
+
 
 if __name__ == "__main__":
     main()
+
+   #if operation == '1':
+      # num1 = input("Enter first number: ")
+      # num2 = input("Enter second number: ")
+      # print("The sum is: " + str(int(num1) + int(num2)))
+   #elif operation == '2':
+      # num1 = input("Enter first number: ")
+      # num2 = input("Enter second number: ")
+      # print("The sum is: " + str(int(num1) - int(num2)))
+   #elif operation == '3':
+      # num1 = input("Enter first number: ")
+      # num2 = input("Enter second number: ")
+      # print("The sum is: " + str(int(num1) *  int(num2)))
+   #elif operation == '4':
+      # num1 = input("Enter first number: ")
+      # num2 = input("Enter second number: ")
+      # print("The sum is: " + str(int(num1) / int(num2))) 
+   #else:
+      # print("Invalid operation")   
+
