a19562eec49a941fee044e191f627b2c9ddd75df
Adrian Aro <aroadrianchristopher@gmail.com>
2025-03-12 21:00:20 +0800

    change the secret num variable

--- secret_number.py
+++ secret_number.py
@@ -1,10 +1,10 @@
 def main():
-    secret_num= 10
+    secret_num= 15
     guess_count = 0
     guess_limit = 6
     print("Try While Looping")
     while guess_count < guess_limit:
-        guess = int(input("Input your guess number 1 to 10: "))
+        guess = int(input("Input your guess number 1 to 15: "))
         guess_count += 1
         if guess == secret_num:
             print("You won!")
