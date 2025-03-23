1ca0688737eb2014a54ceca879c05391f4cf05e6
Adrian Aro <aroadrianchristopher@gmail.com>
2025-03-23 18:55:46 +0800

    edit text in code

--- secret_number.py
+++ secret_number.py
@@ -2,7 +2,7 @@ def main():
     secret_num= 15
     guess_count = 0
     guess_limit = 6
-    print("Try While Looping")
+    print("Try this While Looping")
     while guess_count < guess_limit:
         guess = int(input("Input your guess number 1 to 15: "))
         guess_count += 1
